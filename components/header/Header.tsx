fa334b7416f5831dde0869e091f30ade01406987
nwagu chima <chima.nwagu@decagon.dev>
2022-10-20 18:46:38 +0100

    completed header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -40,7 +40,31 @@ const Header: React.FC<HeaderProps> = ({}) => {
           </div>
         </div>
       </div>
-      <div className={styles.header__mobile}></div>
+      <div className={styles.header__mobile}>
+        <i>
+          <IoSearchOutline />
+        </i>
+        <div className={styles.header__mobile__text}>
+          <h4>Where to?</h4>
+          <p>
+            <span>Anywhere</span>
+            <span>.</span>
+            <span>Any week</span>
+            <span>.</span>
+            <span>Add guests</span>
+          </p>
+        </div>
+        <i className={styles.filter}>
+          <svg
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            role="presentation"
+            focusable="false"
+          >
+            <path d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path>
+          </svg>
+        </i>
+      </div>
     </header>
   );
 };
