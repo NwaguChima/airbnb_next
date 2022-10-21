22538deba9ef15bf860540256c7aeaca0f592e87
nwagu chima <chima.nwagu@decagon.dev>
2022-10-21 04:23:46 +0100

    styled footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,38 +6,47 @@ interface FooterProps {}
 export const Footer: React.FC<FooterProps> = ({}) => {
   return (
     <footer className={styles.footer}>
-      <ul className={styles.footer__support}>
+      <nav aria-label="footer-items" className={styles.footer__support}>
         <h4>Support</h4>
-        <li>Help Center</li>
-        <li>AitCover</li>
-        <li>Safety information</li>
-        <li>Supporting people with disabilities</li>
-        <li>Cancellation options</li>
-        <li>Our COVID-19 Response</li>
-        <li>Report a neighborhood concern</li>
-      </ul>
-      <ul className={styles.footer__community}>
+        <ul>
+          <li>Help Center</li>
+          <li>AitCover</li>
+          <li>Safety information</li>
+          <li>Supporting people with disabilities</li>
+          <li>Cancellation options</li>
+          <li>Our COVID-19 Response</li>
+          <li>Report a neighborhood concern</li>
+        </ul>
+      </nav>
+
+      <nav aria-label="footer-items" className={styles.footer__community}>
         <h4>Community</h4>
-        <li>Airbnb.org disaster relief housing</li>
-        <li>Support Afghan refugees</li>
-        <li>Combating discrimination</li>
-      </ul>
-      <ul className={styles.footer__hosting}>
+        <ul>
+          <li>Airbnb.org disaster relief housing</li>
+          <li>Support Afghan refugees</li>
+          <li>Combating discrimination</li>
+        </ul>
+      </nav>
+      <nav aria-label="footer-items" className={styles.footer__hosting}>
         <h4>Hosting</h4>
-        <li>AirCover for Hosts</li>
-        <li>Explore hosting resources</li>
-        <li>Visit our community forum</li>
-        <li>How to host responsibly</li>
-      </ul>
-      <ul className={styles.footer__airbnb}>
+        <ul>
+          <li>AirCover for Hosts</li>
+          <li>Explore hosting resources</li>
+          <li>Visit our community forum</li>
+          <li>How to host responsibly</li>
+        </ul>
+      </nav>
+      <nav aria-label="footer-items" className={styles.footer__airbnb}>
         <h4>Airbnb</h4>
-        <li>Newsroom</li>
-        <li>Learn about new features</li>
-        <li>Letter from our founders</li>
-        <li>Careers</li>
-        <li>Investors</li>
-        <li>Gift cards</li>
-      </ul>
+        <ul>
+          <li>Newsroom</li>
+          <li>Learn about new features</li>
+          <li>Letter from our founders</li>
+          <li>Careers</li>
+          <li>Investors</li>
+          <li>Gift cards</li>
+        </ul>
+      </nav>
     </footer>
   );
 };
