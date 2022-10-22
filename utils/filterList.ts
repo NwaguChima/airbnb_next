389844090755147076b22d5b7c5b2120254da80d
nwagu chima <chima.nwagu@decagon.dev>
2022-10-22 02:14:37 +0100

    setup nav data

---
+++ utils/filterList.ts
@@ -0,0 +1,102 @@
+export const filterList = [
+  {
+    id: 1,
+    category: "Beach",
+    img: "https://a0.muscache.com/pictures/10ce1091-c854-40f3-a2fb-defc2995bcaf.jpg",
+  },
+  {
+    id: 2,
+    category: "Amazing pools",
+    img: "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
+  },
+  {
+    id: 3,
+    category: "Islands",
+    img: "https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg",
+  },
+  {
+    id: 4,
+    category: "Tropical",
+    img: "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
+  },
+  {
+    id: 5,
+    category: "Caves",
+    img: "https://a0.muscache.com/pictures/4221e293-4770-4ea8-a4fa-9972158d4004.jpg",
+  },
+  {
+    id: 6,
+    category: "OMG!",
+    img: "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    id: 7,
+    category: "Artic",
+    img: "https://a0.muscache.com/pictures/8b44f770-7156-4c7b-b4d3-d92549c8652f.jpg",
+  },
+  {
+    id: 8,
+    category: "Amazing views",
+    img: "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+  },
+  {
+    id: 9,
+    category: "Design",
+    img: "https://a0.muscache.com/pictures/50861fca-582c-4bcc-89d3-857fb7ca6528.jpg",
+  },
+  {
+    id: 10,
+    category: "Surfing",
+    img: "https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg",
+  },
+  {
+    id: 11,
+    category: "Luxe",
+    img: "https://a0.muscache.com/pictures/c8e2ed05-c666-47b6-99fc-4cb6edcde6b4.jpg",
+  },
+  {
+    id: 12,
+    category: "Lake",
+    img: "https://a0.muscache.com/pictures/a4634ca6-1407-4864-ab97-6e141967d782.jpg",
+  },
+  {
+    id: 13,
+    category: "Castles",
+    img: "https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg",
+  },
+  {
+    id: 14,
+    category: "Campers",
+    img: "https://a0.muscache.com/pictures/31c1d523-cc46-45b3-957a-da76c30c85f9.jpg",
+  },
+  {
+    id: 15,
+    category: "Golfing",
+    img: "https://a0.muscache.com/pictures/6b639c8d-cf9b-41fb-91a0-91af9d7677cc.jpg",
+  },
+  {
+    id: 16,
+    category: "Cabins",
+    img: "https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg",
+  },
+  {
+    id: 17,
+    category: "Windmills",
+    img: "https://a0.muscache.com/pictures/5cdb8451-8f75-4c5f-a17d-33ee228e3db8.jpg",
+  },
+  {
+    id: 18,
+    category: "Skiing",
+    img: "https://a0.muscache.com/pictures/c8bba3ed-34c0-464a-8e6e-27574d20e4d2.jpg",
+  },
+  {
+    id: 19,
+    category: "Farms",
+    img: "https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
+  },
+  {
+    id: 20,
+    category: "Towers",
+    img: "https://a0.muscache.com/pictures/d721318f-4752-417d-b4fa-77da3cbc3269.jpg",
+  },
+];
