5cef66f2b2e06b0ea22c8edf5b5385c1df86c5e2
nwagu chima <chima.nwagu@decagon.dev>
2022-10-23 12:16:22 +0100

    added carousel

--- components/mainNav/MainNav.tsx
+++ components/mainNav/MainNav.tsx
@@ -1,13 +1,121 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, A11y, EffectFade } from "swiper";
+import gsap from "gsap";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+
 import styles from "./MainNav.module.scss";
 
 interface MainNavProps {}
 
 export const MainNav: React.FC<MainNavProps> = ({}) => {
+  const scrollRef = useRef<HTMLUListElement>(null);
+  const [scrollX, setScrollX] = useState(0);
+  const [scrollEnd, setScrollEnd] = useState(false);
+
+  const slide = (shift: number) => {
+    scrollRef.current!.scrollLeft += shift;
+    setScrollX(scrollX + shift);
+
+    if (
+      Math.floor(
+        scrollRef.current!.scrollWidth - scrollRef.current!.scrollLeft
+      ) <= scrollRef.current!.clientWidth
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  };
+
+  const anim = (e: { target: any }) => {
+    gsap.from(e.target, { scale: 1 });
+    gsap.to(e.target, { scale: 1.1 });
+  };
+
+  const anim2 = (e: { target: any }) => {
+    gsap.from(e.target, { scale: 1.1 });
+    gsap.to(e.target, { scale: 1 });
+  };
+
+  const scrollCheck = () => {
+    setScrollX(scrollRef.current!.scrollLeft);
+    if (
+      Math.floor(
+        scrollRef.current!.scrollWidth - scrollRef.current!.scrollLeft
+      ) <= scrollRef.current!.offsetWidth
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  };
+
   return (
     <div className={styles.navContainer}>
       <nav aria-label="main-nav" className={styles.navContainer__nav}>
-        Test
+        {
+          <button
+            onClick={() => slide(-200)}
+            onMouseEnter={(e) => anim(e)}
+            onMouseLeave={(e) => anim2(e)}
+            style={{ opacity: scrollX === 0 ? 0 : 1 }}
+          >
+            <i>
+              <MdKeyboardArrowLeft size={20} />
+            </i>
+          </button>
+        }
+
+        <ul ref={scrollRef} onScroll={scrollCheck}>
+          <li>Hello 1</li>
+          <li>Hello 2</li>
+          <li>Hello 3</li>
+          <li>Hello 4</li>
+          <li>Hello 5</li>
+          <li>Hello 6</li>
+          <li>Hello 7</li>
+          <li>Hello 8</li>
+          <li>Hello 9</li>
+          <li>Hello 10</li>
+          <li>Hello 1</li>
+          <li>Hello 2</li>
+          <li>Hello 3</li>
+          <li>Hello 4</li>
+          <li>Hello 5</li>
+          <li>Hello 6</li>
+          <li>Hello 7</li>
+          <li>Hello 8</li>
+          <li>Hello 9</li>
+          <li>Hello 10</li>
+          <li>Hello 1</li>
+          <li>Hello 2</li>
+          <li>Hello 3</li>
+          <li>Hello 4</li>
+          <li>Hello 5</li>
+          <li>Hello 6</li>
+          <li>Hello 7</li>
+          <li>Hello 8</li>
+          <li>Hello 9</li>
+          <li>Hello 10</li>
+        </ul>
+
+        {
+          <button
+            onClick={() => slide(+200)}
+            onMouseEnter={(e) => anim(e)}
+            onMouseLeave={(e) => anim2(e)}
+            style={{ opacity: scrollEnd ? 0 : 1 }}
+          >
+            <i>
+              <MdKeyboardArrowRight size={20} />
+            </i>
+          </button>
+        }
       </nav>
       <div className={styles.navContainer__filter}>
         <i>
